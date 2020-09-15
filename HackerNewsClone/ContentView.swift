74ae54a0e91f124d35d9cd98c0072899b8eb0f80
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-15 15:35:06 +0300

    Initial Commit

---
+++ HackerNewsClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNewsClone
+//
+//  Created by ramil on 15.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackerNewsClone/HackerNewsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsCloneApp.swift
+//  HackerNewsClone
+//
+//  Created by ramil on 15.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
